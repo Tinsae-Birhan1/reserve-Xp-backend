a58ac6172b1ce7557c0bb1f0c3af7d129b0c4615
Debisa07 <debisaabebesa@gmail.com>
2023-11-21 22:20:04 +0300

    car crud :

--- server.js
+++ server.js
@@ -9,10 +9,87 @@ app.getMaxListeners('/', (req, res)=>{
 })
 
 
+// GET all cars
+app.get("/cars", async (req, res) => {
+    try {
+        const cars = await Car.find({});
+        res.status(200).json(cars);
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({ message: error.message });
+    }
+});
 
-app.listen(4000, ()=>{
-    console.log("Node API app is running on Port 4000")
-})
+// GET a single car by ID
+app.get("/cars/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const car = await Car.findById(id);
+
+        if (!car) {
+            return res.status(404).json({ message: "Car not found" });
+        }
+
+        res.status(200).json(car);
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({ message: error.message });
+    }
+});
+
+// CREATE a new car
+app.post("/cars", async (req, res) => {
+    try {
+        const newCar = await Car.create(req.body);
+        res.status(201).json(newCar);
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({ message: error.message });
+    }
+});
+
+// UPDATE a car by ID
+app.put("/cars/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const updatedCar = await Car.findByIdAndUpdate(id, req.body, { new: true });
+
+        if (!updatedCar) {
+            return res.status(404).json({ message: "Car not found" });
+        }
+
+        res.status(200).json(updatedCar);
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({ message: error.message });
+    }
+});
+
+// DELETE a car by ID
+app.delete("/cars/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const deletedCar = await Car.findByIdAndDelete(id);
+
+        if (!deletedCar) {
+            return res.status(404).json({ message: "Car not found" });
+        }
+
+        res.status(200).json({ message: "Car deleted successfully" });
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({ message: error.message });
+    }
+});
+
+// ...existing code...
+
+app.listen(4000, () => {
+    console.log("Node API app is running on Port 4000");
+});
+
+
+
 app.get("/hotel", async(req, res)=>{
     // console.log(req.body)
     // res.send(req.body)
