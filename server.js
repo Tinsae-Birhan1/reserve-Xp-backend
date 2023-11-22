84eacaa91878a27bd79cb7c0fa0d3d7349a1419a
Tinsae-Birhan1 <tinsaebirhan7@gmail.com>
2023-11-22 10:42:36 +0300

    added tour module

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const Boat = require('./models/boatModels')
 const Event = require('./models/eventModels')
 const Flight = require('./models/flightModels')
 const Space = require('./models/spaceModels')
+const Tour = require('./models/TourModels');
+
 const cors = require('cors');
 const app = express();
 
@@ -439,7 +441,41 @@ app.post("/hotel", async(req, res)=>{
     }
 })
 
+app.get("/tour", async (req, res) => {
+    try {
+        const newTour = await Tour.find({});
+        res.status(200).json(newTour);
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({ message: error.message });
+    }
+});
 
+app.get("/tour/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const tour = await Tour.findById(id);
+
+        if (!tour) {
+            return res.status(404).json({ message: "Tour not found" });
+        }
+
+        res.status(200).json(tour);
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({ message: error.message });
+    }
+});
+
+app.post("/tour", async (req, res) => {
+    try {
+        const newTour = await Tour.create(req.body);
+        res.status(200).json(newTour);
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({ message: error.message });
+    }
+});
 mongoose.
 connect("mongodb+srv://tinsaebirhan7:D9EubbEi5lJpDCW1@cluster0.ovqcrvw.mongodb.net/ReserveXP?retryWrites=true&w=majority")
 .then(()=> {
