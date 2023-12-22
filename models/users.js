40d186cbc99fd6be0f8d0624374fbb7bb84bb1d0
Mesi Abeza <tinsaebirhan7@gmail.com>
2023-12-22 12:25:48 +0300

    added vendor and admin can reject, accept

--- models/users.js
+++ models/users.js
@@ -3,7 +3,9 @@ const {Schema, model} = require("../database/connection") // import Schema & mod
 // User Schema
 const UserSchema = new Schema({
     username: {type: String, unique: true, required: true},
-    password: {type: String, required: true}
+    password: {type: String, required: true},
+    role: { type: String, required: true }, // Add role field
+    status: { type: String, default: null, required: function () { return this.role === 'vendor'; } },
 })
 
 // User model
