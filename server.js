8df4634656220b7387c657966132f458f3350424
Mesi Abeza <tinsaebirhan7@gmail.com>
2023-12-22 10:13:26 +0300

    authentication

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+<<<<<<< HEAD
 const mongoose = require('mongoose')
 const Hotel = require('./models/hotelModels')
 const Car = require('./models/carModels')
@@ -19,6 +20,21 @@ const app = express();
 require("dotenv").config() // load .env variables
 app.use(morgan("tiny")) // log the request for debugging
 app.use(express.json()) // parse json bodies
+=======
+const { default: mongoose } = require('mongoose')
+const Booking = require('./models/bookingModels');
+
+
+const Roomrouter = require('./routes/rooms');
+const flightRoutes = require('./routes/flight');
+const spaceRoutes = require('./routes/space');
+const eventRoutes = require('./routes/event');
+const BoatRoutes = require('./routes/boat');
+const carRoutes = require('./routes/car');
+const hotelRoutes = require('./routes/hotel');
+const tourRouter = require('./routes/tour');
+const roomRouter = require('./routes/room');
+>>>>>>> d136564bc5869f87e6a558cedb102c51c31d8e46
 
 const cors = require('cors');
 
@@ -31,386 +47,20 @@ app.getMaxListeners('/', (req, res) => {
 
 })
 
+// Routes
+app.use('/', flightRoutes);
+app.use('/', spaceRoutes);
+app.use('/',eventRoutes);
+app.use('/',BoatRoutes);
+app.use('/',carRoutes);
+app.use('/',hotelRoutes);
+app.use('/',tourRouter);
+app.use('/',roomRouter);
 
-// GET all Flight
-
-app.get("/flights", async (req, res) => {
-    try {
-        const flights = await Flight.find({});
-        res.status(200).json(flights);
-    } catch (error) {
-        console.log(error.message);
-        res.status(500).json({ message: error.message });
-    }
-});
-
-// GET a single flight by ID
-app.get("/flights/:id", async (req, res) => {
-    try {
-        const { id } = req.params;
-        const flight = await Flight.findById(id);
-
-        if (!flight) {
-            return res.status(404).json({ message: "Flight not found" });
-        }
-
-        res.status(200).json(flight);
-    } catch (error) {
-        console.log(error.message);
-        res.status(500).json({ message: error.message });
-    }
-});
-
-// CREATE a new flight
-app.post("/flights", async (req, res) => {
-    try {
-        const newFlight = await Flight.create(req.body);
-        res.status(201).json(newFlight);
-    } catch (error) {
-        console.log(error.message);
-        res.status(500).json({ message: error.message });
-    }
-});
-
-
-// UPDATE a flight by ID
-app.put("/flights/:id", async (req, res) => {
-    try {
-        const { id } = req.params;
-        const updatedFlight = await Flight.findByIdAndUpdate(id, req.body, { new: true });
-
-        if (!updatedFlight) {
-            return res.status(404).json({ message: "Flight not found" });
-        }
-
-        res.status(200).json(updatedFlight);
-    } catch (error) {
-        console.log(error.message);
-        res.status(500).json({ message: error.message });
-    }
-});
-
-// DELETE a flight by ID
-
-app.delete("/flights/:id", async (req, res) => {
-    try {
-        const { id } = req.params;
-        const deletedFlight = await Flight.findByIdAndDelete(id);
-
-        if (!deletedFlight) {
-            return res.status(404).json({ message: "Flight not found" });
-        }
-
-        res.status(200).json({ message: "Flight deleted successfully" });
-    } catch (error) {
-        console.log(error.message);
-        res.status(500).json({ message: error.message });
-    }
-});
-
-// GET all spaces
-app.get("/spaces", async (req, res) => {
-    try {
-        const spaces = await Space.find({});
-        res.status(200).json(spaces);
-    } catch (error) {
-        console.log(error.message);
-        res.status(500).json({ message: error.message });
-    }
-});
-
-// GET a single space by ID
-app.get("/spaces/:id", async (req, res) => {
-    try {
-        const { id } = req.params;
-        const space = await Space.findById(id);
-
-        if (!space) {
-            return res.status(404).json({ message: "Space not found" });
-        }
-
-        res.status(200).json(space);
-    } catch (error) {
-        console.log(error.message);
-        res.status(500).json({ message: error.message });
-    }
-});
-
-// CREATE a new space
-app.post("/spaces", async (req, res) => {
-    try {
-        const newSpace = await Space.create(req.body);
-        res.status(201).json(newSpace);
-    } catch (error) {
-        console.log(error.message);
-        res.status(500).json({ message: error.message });
-    }
-});
-
-// UPDATE a space by ID
-app.put("/spaces/:id", async (req, res) => {
-    try {
-        const { id } = req.params;
-        const updatedSpace = await Space.findByIdAndUpdate(id, req.body, { new: true });
-
-        if (!updatedSpace) {
-            return res.status(404).json({ message: "Space not found" });
-        }
-
-        res.status(200).json(updatedSpace);
-    } catch (error) {
-        console.log(error.message);
-        res.status(500).json({ message: error.message });
-    }
-});
-
-// DELETE a space by ID
-app.delete("/spaces/:id", async (req, res) => {
-    try {
-        const { id } = req.params;
-        const deletedSpace = await Space.findByIdAndDelete(id);
-
-        if (!deletedSpace) {
-            return res.status(404).json({ message: "Space not found" });
-        }
-
-        res.status(200).json({ message: "Space deleted successfully" });
-    } catch (error) {
-        console.log(error.message);
-        res.status(500).json({ message: error.message });
-    }
-});
-
-
-
-// GET all events
-app.get("/events", async (req, res) => {
-    try {
-        const events = await Event.find({});
-        res.status(200).json(events);
-    } catch (error) {
-        console.log(error.message);
-        res.status(500).json({ message: error.message });
-    }
-});
-
-// CREATE a new event
-app.post("/events", async (req, res) => {
-    try {
-        const newEvent = await Event.create(req.body);
-        res.status(201).json(newEvent);
-    } catch (error) {
-        console.log(error.message);
-        res.status(500).json({ message: error.message });
-    }
-});
-
-// GET a single event by ID
-app.get("/events/:id", async (req, res) => {
-    try {
-        const { id } = req.params;
-        const event = await Event.findById(id);
-
-        if (!event) {
-            return res.status(404).json({ message: "Event not found" });
-        }
-
-        res.status(200).json(event);
-    } catch (error) {
-        console.log(error.message);
-        res.status(500).json({ message: error.message });
-    }
-});
-
-// UPDATE an event by ID
-app.put("/events/:id", async (req, res) => {
-    try {
-        const { id } = req.params;
-        const updatedEvent = await Event.findByIdAndUpdate(id, req.body, { new: true });
-
-        if (!updatedEvent) {
-            return res.status(404).json({ message: "Event not found" });
-        }
-
-        res.status(200).json(updatedEvent);
-    } catch (error) {
-        console.log(error.message);
-        res.status(500).json({ message: error.message });
-    }
-});
-
-// DELETE an event by ID
-app.delete("/events/:id", async (req, res) => {
-    try {
-        const { id } = req.params;
-        const deletedEvent = await Event.findByIdAndDelete(id);
-
-        if (!deletedEvent) {
-            return res.status(404).json({ message: "Event not found" });
-        }
-
-        res.status(200).json({ message: "Event deleted successfully" });
-    } catch (error) {
-        console.log(error.message);
-        res.status(500).json({ message: error.message });
-    }
-});
-
-
-
-// GET all boats
-app.get("/boats", async (req, res) => {
-    try {
-        const boats = await Boat.find({});
-        res.status(200).json(boats);
-    } catch (error) {
-        console.log(error.message);
-        res.status(500).json({ message: error.message });
-    }
-});
-
-// GET a single boat by ID
-app.get("/boats/:id", async (req, res) => {
-    try {
-        const { id } = req.params;
-        const boat = await Boat.findById(id);
-
-        if (!boat) {
-            return res.status(404).json({ message: "Boat not found" });
-        }
-
-        res.status(200).json(boat);
-    } catch (error) {
-        console.log(error.message);
-        res.status(500).json({ message: error.message });
-    }
-});
-
-// CREATE a new boat
-app.post("/boats", async (req, res) => {
-    try {
-        const newBoat = await Boat.create(req.body);
-        res.status(201).json(newBoat);
-    } catch (error) {
-        console.log(error.message);
-        res.status(500).json({ message: error.message });
-    }
-});
-
-// UPDATE a boat by ID
-app.put("/boats/:id", async (req, res) => {
-    try {
-        const { id } = req.params;
-        const updatedBoat = await Boat.findByIdAndUpdate(id, req.body, { new: true });
-
-        if (!updatedBoat) {
-            return res.status(404).json({ message: "Boat not found" });
-        }
-
-        res.status(200).json(updatedBoat);
-    } catch (error) {
-        console.log(error.message);
-        res.status(500).json({ message: error.message });
-    }
-});
-
-// DELETE a boat by ID
-app.delete("/boats/:id", async (req, res) => {
-    try {
-        const { id } = req.params;
-        const deletedBoat = await Boat.findByIdAndDelete(id);
-
-        if (!deletedBoat) {
-            return res.status(404).json({ message: "Boat not found" });
-        }
-
-        res.status(200).json({ message: "Boat deleted successfully" });
-    } catch (error) {
-        console.log(error.message);
-        res.status(500).json({ message: error.message });
-    }
-});
-
-
-
-// GET all cars
-app.get("/cars", async (req, res) => {
-    try {
-        const cars = await Car.find({});
-        res.status(200).json(cars);
-    } catch (error) {
-        console.log(error.message);
-        res.status(500).json({ message: error.message });
-    }
-});
-
-// GET a single car by ID
-app.get("/cars/:id", async (req, res) => {
-    try {
-        const { id } = req.params;
-        const car = await Car.findById(id);
-
-        if (!car) {
-            return res.status(404).json({ message: "Car not found" });
-        }
-
-        res.status(200).json(car);
-    } catch (error) {
-        console.log(error.message);
-        res.status(500).json({ message: error.message });
-    }
-});
-
-// CREATE a new car
-app.post("/cars", async (req, res) => {
-    try {
-        const newCar = await Car.create(req.body);
-        res.status(201).json(newCar);
-    } catch (error) {
-        console.log(error.message);
-        res.status(500).json({ message: error.message });
-    }
-});
-
-// DELETE a car by ID
-app.delete("/cars/:id", async (req, res) => {
-    try {
-        const { id } = req.params;
-        const deletedCar = await Car.findByIdAndDelete(id);
-
-        if (!deletedCar) {
-            return res.status(404).json({ message: "Car not found" });
-        }
-
-        res.status(200).json({ message: "Car deleted successfully" });
-    } catch (error) {
-        console.log(error.message);
-        res.status(500).json({ message: error.message });
-    }
-});
-
-// UPDATE a car by ID
-app.put("/cars/:id", async (req, res) => {
-    try {
-        const { id } = req.params;
-        const updatedCar = await Car.findByIdAndUpdate(id, req.body, { new: true });
-
-        if (!updatedCar) {
-            return res.status(404).json({ message: "Car not found" });
-        }
-
-        res.status(200).json(updatedCar);
-    } catch (error) {
-        console.log(error.message);
-        res.status(500).json({ message: error.message });
-    }
-});
-
-
-// GET all hotels
 app.listen(4000, () => {
     console.log("Node API app is running on Port 4000");
 });
+<<<<<<< HEAD
 
 app.get("/hotel", async (req, res) => {
     // console.log(req.body)
@@ -490,10 +140,14 @@ app.post("/tour", async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 });
+=======
+mongoose.
+    connect("mongodb+srv://tinsaebirhan7:D9EubbEi5lJpDCW1@cluster0.ovqcrvw.mongodb.net/ReserveXP?retryWrites=true&w=majority")
+    .then(() => {
+        app.getMaxListeners('/blog', (req, res) => {
+            res.send("Hello Node API")
+>>>>>>> d136564bc5869f87e6a558cedb102c51c31d8e46
 
-
-// CREATE a new booking for a room
-app.use("/rooms", Roomrouter);
 
 
 // GET all bookings
@@ -564,6 +218,7 @@ app.delete("/bookings/:id", async (req, res) => {
 });
 
 // GET all rooms
+<<<<<<< HEAD
 app.get("/rooms", async (req, res) => {
     try {
         const rooms = await Room.find({});
@@ -633,3 +288,5 @@ app.delete("/rooms/:id", async (req, res) => {
     }
 }
 );
+=======
+>>>>>>> d136564bc5869f87e6a558cedb102c51c31d8e46
