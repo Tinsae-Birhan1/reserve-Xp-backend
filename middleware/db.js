88379f3f6f3f4d17d0334f07ebae40f3f377c195
HuluWZ <hulunlante.w@gmail.com>
2024-01-16 18:04:51 +0300

    Add middleware for connecting to tenant and central databases

---
+++ middleware/db.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const Tenant = require('../tenant/tenant')
+
+const connectToDatabase = async ( dbName) => {
+    try {
+        await mongoose.connection.close();
+        await mongoose.connect(
+            `mongodb+srv://${process.env.MONGODB_ACCOUNT}:${process.env.MONGODB_PASS}${process.env.MONGODB_CLUSTER}/${dbName}?retryWrites=true&w=majority`
+        );
+        console.log(`Connected to database: ${dbName}`);
+    } catch (error) {
+        console.error(`Error connecting to the database ${dbName}:`, error);
+        throw error; // Rethrow the error for the calling function to handle
+    }
+};
+
+const tenantDb = async function (req, res, next) {
+    const {slug} = req.params
+    console.log('Tenant Database')
+    console.log(" Slug ", slug)
+    await connectToDatabase(slug);
+    next()
+}
+const centralDb = async function (req, res, next) {
+    console.log('Central Database')
+    await connectToDatabase(process.env.DB_NAME);
+    next()
+}
+module.exports = {tenantDb , centralDb};
