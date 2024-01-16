19a0364409d1965cad182dcf603b5d985006cf2d
HuluWZ <hulunlante.w@gmail.com>
2024-01-16 18:05:36 +0300

    Add auth middleware for database connection and authentication

---
+++ middleware/auth.js
@@ -0,0 +1,35 @@
+const Tenant = require('../tenant/tenant')
+const mongoose = require("mongoose");
+
+const connectToDatabase = async (dbName) => {
+    try {
+        await mongoose.connection.close();
+        await mongoose.connect(
+            `mongodb+srv://${process.env.MONGODB_ACCOUNT}:${process.env.MONGODB_PASS}${process.env.MONGODB_CLUSTER}/${dbName}?retryWrites=true&w=majority`
+        );
+        console.log(`Connected to database: ${dbName}`);
+    } catch (error) {
+        console.error('Error connecting to the database:', error);
+        throw error; // Rethrow the error for the calling function to handle
+    }
+};
+const auth = async (req, res, next) => {
+    // await connectToDatabase(process.env.DB_NAME);
+    const doesItExist = await Tenant.findOne({ business_name: req.params.slug });
+    console.log(" Tenant ", doesItExist);
+    if(doesItExist){
+      req.database = doesItExist
+      req.user = req.params.slug
+      // await mongoose.connect( `mongodb+srv://${process.env.MONGODB_ACCOUNT}:${process.env.MONGODB_PASS}${process.env.MONGODB_CLUSTER}/${req.params.slug}?retryWrites=true&w=majority`);
+      next()
+    }else{
+      return res.status(403).json({message:"Tenant does not found~"})
+    }
+}
+
+const centralAuth = async  (req, res, next) =>{
+  console.log(' Central Auth ', req.body)
+  await connectToDatabase(process.env.DB_NAME);
+  next()
+}
+module.exports = {auth , centralAuth};
